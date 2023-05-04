4b46d87f550a90c0820f4003e1a7e75a2353f41c
Z01Y <zhang.ziyue99@gmail.com>
2023-05-04 16:33:50 -0400

    Update About section and reusable cards

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import AboutImage from '../../assets/ME.png';
+import CV_SWE from '../../assets/Ziyue(Zoey)ZhangResume_SWE.pdf';
+import CV_PM from '../../assets/Ziyue(Zoey)ZhangResume_PM.pdf';
+import { HiDownload } from 'react-icons/hi';
+import data from './data';
+import Card from '../../components/Card';
 import './about.css';
 const About = () => {
   return (
     <section id="about">
-      <h2>About</h2>
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards">
+            {data.map((item) => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
+          </div>
+          <p>
+            I have always had a deep passion for building projects. Throughout
+            my college years and after it, I gained more than 3 years of
+            experience in web development and I am constantly learning new
+            skills in this field. Apart from web development, I am also
+            interested in product management and I am actively practicing it. My
+            motivation to achieve more is limitless!
+          </p>
+          <p>
+            Greetings! My name is Ziyue Zhang, and I am from Shanghai, China.
+            You may refer to me as Zoey. I recently graduated with a Bachelor's
+            degree in Informatics in 2022. My career path is focused on
+            full-stack web development and product management, and I have
+            experience in both areas. Get in touch with me and Check out my
+            resume and projects below!
+          </p>
+          <a href={CV_SWE} download className="btn primary">
+            Download SWE CV <HiDownload />
+          </a>
+          <a href={CV_PM} download className="btn primary">
+            Download PM CV <HiDownload />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/sections/about/data.js
@@ -0,0 +1,19 @@
+import { FaAward } from 'react-icons/fa';
+import { TbBooks } from 'react-icons/tb';
+
+const data = [
+  {
+    id: 1,
+    icon: <FaAward />,
+    title: 'Experience',
+    desc: '3+ Years Experience',
+  },
+  {
+    id: 2,
+    icon: <TbBooks />,
+    title: 'Projects',
+    desc: 'Working on more projects',
+  },
+];
+
+export default data;
