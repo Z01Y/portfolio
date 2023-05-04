1ee58cf5bfd06222cf8944ee554bdf4458432304
Z01Y <zhang.ziyue99@gmail.com>
2023-05-03 23:19:52 -0400

    Header section

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,11 +1,39 @@
+import HeaderImage from '../../assets/header1.jpg';
+import data from './data';
 import React from 'react';
 import './header.css';
 
 const Header = () => {
   return (
-    <section id="header">
-      <h2>Header</h2>
-    </section>
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portrait"></img>
+        </div>
+        <h3>Zoey Zhang</h3>
+        <p>Hi</p>
+        <div className="header__cta">
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
+        </div>
+        <div className="header__socials">
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </header>
   );
 };
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
+
+const data = [
+  {
+    id: 1,
+    link: 'https://www.instagram.com/9907_y/',
+    icon: <AiOutlineInstagram />,
+  },
+  { id: 1, link: 'https://github.com/Z01Y', icon: <AiOutlineGithub /> },
+];
+
+export default data;
